cac200110ef037005f6a8d863ea5a84410a6783a
baby.shark <babyshark1234@gmail.com>
2020-11-17 13:57:26 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
- before_action :set_task, only: [:show, :edit, :update, :destroy]
+ before_action :require_user_logged_in
+ before_action :correct_user, only: [:destroy, :edit, :show , :update]
     
  def index
     @tasks = Task.all
  end
  
  def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
      flash[:success] = 'Task が正常に投稿されました'
      redirect_to @task
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
  end
 
  def new
-     @task = Task.new
+   @task = Task.new
  end
  
  def edit
@@ -28,7 +28,6 @@ class TasksController < ApplicationController
  end
  
  def update
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,18 +39,21 @@ class TasksController < ApplicationController
  
  def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
  end
  
  private
- 
- def set_task
-    @task = Task.find(params[:id])
- end
 
  def task_params
   params.require(:task).permit(:content, :status)
  end
+ 
+ def correct_user
+   @task = current_user.tasks.find_by(id: params[:id])
+   unless @task
+     redirect_to root_url
+   end
+ end
+
 end
